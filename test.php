3b10c8e8dc2670341cb07ae080e0383c02e3bc36
nonoji08 <nonoji08@naver.com>
2016-09-01 10:28:42 +0900

    269

---
+++ test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="Generator" content="EditPlus">
+        <meta name="Author" content="">
+        <meta name="Keywords" content="">
+        <meta name="Description" content="">
+        <title>솔져</title>
+    </head>
+    <body>
+       <ul>
+       <li>1</li>
+        <?php
+            print "안녕 안녕 안녕하세요";
+        ?>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        </ul>
+    </body>
+</html>
