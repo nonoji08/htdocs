90c00ba7b1f47945cfc16fe8b9ea893649ea237f
nonoji08 <nonoji08@naver.com>
2016-09-08 11:32:22 +0900

    엄지갓

---
+++ confirm.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+    
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>일천팍</title>
+    </head>
+    <body>
+        <?php
+            
+        ?>
+    </body>
+</html>
